e5377dd52eca404baff19acf3f437f33a520ed15
4rnol <arnol01233@gmail.com>
2024-04-04 08:58:57 -0400

    add repository file

---
+++ Lumina-BackEnd/Repository/User/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using Lumina_BackEnd.Models;
+
+namespace Lumina_BackEnd.Repository.User
+{
+    public interface IUserRepository
+    {
+        Task<Models.User> GetUser(int id);
+        Task<Models.User> AddUser(Models.User usr);
+        Task<List<Models.User>> GetUsers();
+    }
+}
---
+++ Lumina-BackEnd/Repository/User/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using Lumina_BackEnd.Data;
+
+namespace Lumina_BackEnd.Repository.User
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApiDbContext _context;
+
+        public UserRepository(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Models.User> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<Models.User> AddUser(Models.User usr)
+        {
+            _context.Users.Add(usr);
+            await _context.SaveChangesAsync();
+            return usr;
+        }
+
+        public async Task<List<Models.User>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+    }
+}
