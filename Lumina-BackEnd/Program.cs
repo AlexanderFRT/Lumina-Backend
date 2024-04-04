94e09e5d2bdee327b2fd95dfeff8f793336c2180
AlexanderFRT <alexyetz@gmail.com>
2024-04-03 20:48:04 -0600

    Completada la configuración base de conexión entre el DB y el EF

--- Lumina-BackEnd/Program.cs
+++ Lumina-BackEnd/Program.cs
@@ -1,4 +1,7 @@
 
+using Lumina_BackEnd.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace Lumina_BackEnd
 {
     public class Program
@@ -13,6 +16,9 @@ namespace Lumina_BackEnd
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddEntityFrameworkNpgsql()
+                .AddDbContext<ApiDbContext>(opt => 
+                opt.UseNpgsql(builder.Configuration.GetConnectionString("LuminaDbConnection")));
 
             var app = builder.Build();
 
