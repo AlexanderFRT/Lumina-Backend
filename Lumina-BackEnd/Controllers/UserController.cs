019869e92802092422cfab24b159251b2fbcb95a
4rnol <arnol01233@gmail.com>
2024-04-04 23:59:01 -0400

    userController

---
+++ Lumina-BackEnd/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Lumina_BackEnd.Repository.User;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Lumina_BackEnd.Models;
+
+namespace Lumina_BackEnd.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        
+
+        [HttpPost]
+        public async Task<ActionResult> AddUser(User user)
+        {
+            return Ok(await _userRepository.AddUser(user));
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _userRepository.GetUsers());
+        }
+
+    }
+}
