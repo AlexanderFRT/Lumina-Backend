3d8c4ba095ceccf314ed3e323dbfb8b1ee2f76bb
AlexanderFRT <alexyetz@gmail.com>
2024-04-04 22:14:15 -0600

    Agregada la configuración de las entidades

--- Lumina-BackEnd/Data/ApiDbContext.cs
+++ Lumina-BackEnd/Data/ApiDbContext.cs
@@ -15,26 +15,46 @@ namespace Lumina_BackEnd.Data
             base.OnModelCreating(builder);
 
             builder.Entity<Account>()
-                .ToTable("Account");        
-            
+                .ToTable("Accounts");
+
             builder.Entity<Log>()
-                .ToTable("Log");
-            
+                .ToTable("Logs");
+
             builder.Entity<Security>()
-                .ToTable("Security");
+                .ToTable("Securities");
 
             builder.Entity<User>()
-                .ToTable("User"); 
-            
+                .ToTable("Users");
+
             builder.Entity<Transaction>()
-                .ToTable("Transaction");
+                .ToTable("Transactions");
+
+            // Define las foreign keys
+            builder.Entity<Account>()
+                .HasOne(a => a.User)
+                .WithMany(u => u.Accounts)
+                .HasForeignKey(a => a.UserID);
+
+            builder.Entity<Security>()
+                .HasOne(s => s.User)
+                .WithMany(u => u.Securities)
+                .HasForeignKey(s => s.UserId);
+
+            builder.Entity<Log>()
+                .HasOne(l => l.User)
+                .WithMany(u => u.Logs)
+                .HasForeignKey(l => l.UserId);
+
+            builder.Entity<Transaction>()
+                .HasOne(t => t.Account)
+                .WithMany(a => a.Transactions)
+                .HasForeignKey(t => t.AccountID);
         }
 
-        public DbSet<Account>? Account { get; set; }
-        public DbSet<Log>? Logs { get; set; }
-        public DbSet<Security>? Security { get; set; }
-        public DbSet<User>? User { get; set; }
-        public DbSet<Transaction>? Transaction { get; set; }
-
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Log> Logs { get; set; }
+        public DbSet<Security> Securities { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
     }
 }
