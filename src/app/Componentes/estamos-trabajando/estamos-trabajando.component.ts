c04b1558da849d3690c6d0295ccf421ae35f3fd1
Karla Chavez <karla.tkm.96@gmail.com>
2024-04-08 14:03:17 -0300

    AddFooterAndEstamosTrabajando

---
+++ src/app/Componentes/estamos-trabajando/estamos-trabajando.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-estamos-trabajando',
+  standalone: true,
+  imports: [],
+  templateUrl: './estamos-trabajando.component.html',
+  styleUrl: './estamos-trabajando.component.css'
+})
+export class EstamosTrabajandoComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { EstamosTrabajandoComponent } from './Componentes/estamos-trabajando/estamos-trabajando.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'EstamosTrabajando',
+        component:EstamosTrabajandoComponent
+    },
+    {
+        path:'',
+        redirectTo:"",
+        pathMatch:'full'
+    },
+    
+];
