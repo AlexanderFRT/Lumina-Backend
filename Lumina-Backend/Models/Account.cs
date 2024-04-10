f2a413004664f1579c69e7a94fcba79729e92b1a
4rnol <arnol01233@gmail.com>
2024-04-09 22:15:01 -0400

    new migration

--- Lumina-Backend/Models/Account.cs
+++ Lumina-Backend/Models/Account.cs
@@ -3,13 +3,13 @@
 public class Account : BaseEntity
 {
     public int UserID { get; set; }
-    public User User { get; set; }
+    public virtual User User { get; set; }
 
     public AccountType Type { get; set; }
     public decimal Balance { get; set; }
     public int AccountNumber { get; set; }
 
-    public ICollection<Transaction> Transactions { get; set; }
+    public virtual ICollection<Transaction> Transactions { get; set; }
 }
 
 public enum AccountType
