02f29f0654c75ab5d70e942a9d46e576b9b71ed0
AlexanderFRT <alexyetz@gmail.com>
2024-04-09 20:48:06 -0600

    Arreglada la variable de entorno para conectar a la DB remota temporalmente

--- Lumina-Backend/Program.cs
+++ Lumina-Backend/Program.cs
@@ -3,40 +3,44 @@ using Lumina_Backend.Data;
 using Lumina_Backend.Repository.User;
 using Microsoft.EntityFrameworkCore;
 
-namespace Lumina_Backend
+namespace Lumina_Backend;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+
+        var builder = WebApplication.CreateBuilder(args);
+        var connectionString = Environment.GetEnvironmentVariable("DATABASE_URL");
+     
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddEntityFrameworkNpgsql()
+            .AddDbContext<ApiDbContext>(opt =>
+                opt.UseNpgsql(connectionString));
+
+        //  Cuando ya se vaya a hacer el despliegue local en Docker se elimina la env variable connectionString y se modifica el servicio nuevamente con el código de abajo, y se habilita el string del appsettings.json
+        //      opt.UseNpgsql(builder.Configuration.GetConnectionString("LuminaConnection")));
+
+        builder.Services.AddTransient<IUserRepository, UserRepository>();
+        builder.Services.AddAuthorization();
+        builder.Services.AddAuthentication("Bearer").AddJwtBearer();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddEntityFrameworkNpgsql()
-                .AddDbContext<ApiDbContext>(opt => 
-                opt.UseNpgsql(builder.Configuration.GetConnectionString("LuminaDbConnection")));
-            builder.Services.AddTransient<IUserRepository, UserRepository>();
-            builder.Services.AddAuthorization();
-            builder.Services.AddAuthentication("Bearer").AddJwtBearer();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseAuthorization();
-            app.MapControllers();
-            app.Run();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+        app.Run();
     }
 }
