2a040b3011d8f76fc91e3dff45af3ab1aeffc8fe
AlexanderFRT <alexyetz@gmail.com>
2024-04-10 15:50:52 -0600

    Arreglado el orden en el ORM para que la tabla del User sea la primera en ser creada, y no requiera dependencias

--- Lumina-Backend/Data/ApiDbContext.cs
+++ Lumina-Backend/Data/ApiDbContext.cs
@@ -14,8 +14,14 @@ namespace Lumina_Backend.Data
         {
             base.OnModelCreating(builder);
 
+            builder.Entity<User>()
+                .ToTable("Users");
+
             builder.Entity<Account>()
                 .ToTable("Accounts");
+
+            builder.Entity<Transaction>()
+                .ToTable("Transactions");
 
             builder.Entity<Log>()
                 .ToTable("Logs");
@@ -23,17 +29,16 @@ namespace Lumina_Backend.Data
             builder.Entity<Security>()
                 .ToTable("Securities");
 
-            builder.Entity<User>()
-                .ToTable("Users");
-
-            builder.Entity<Transaction>()
-                .ToTable("Transactions");
-
             // Define las foreign keys
             builder.Entity<Account>()
                 .HasOne(a => a.User)
                 .WithMany(u => u.Accounts)
                 .HasForeignKey(a => a.UserID);
+
+            builder.Entity<Transaction>()
+                .HasOne(t => t.Account)
+                .WithMany(a => a.Transactions)
+                .HasForeignKey(t => t.AccountID);
 
             builder.Entity<Security>()
                 .HasOne(s => s.User)
@@ -45,16 +50,22 @@ namespace Lumina_Backend.Data
                 .WithMany(u => u.Logs)
                 .HasForeignKey(l => l.UserId);
 
-            builder.Entity<Transaction>()
-                .HasOne(t => t.Account)
-                .WithMany(a => a.Transactions)
-                .HasForeignKey(t => t.AccountID);
-        }
+            var demoUsers = new List<User>
+            {
+            new User { Id = -1, UserName = "ajruiz2204", Password = "123456", Email = "ajruiz2204@example.com"},
+            new User { Id = -2, UserName = "4rnol", Password = "123456", Email = "4rnol@example.com"},
+            new User { Id = -3, UserName = "AlexanderFRT", Password = "123456", Email = "alexanderfrt@example.com"},
+            new User { Id = -4, UserName = "ezealeguzman", Password = "123456", Email = "ezealeguzman@example.com"},
+            new User { Id = -5, UserName = "giolucc", Password = "123456", Email = "giolucc@example.com"}
+            };
+            builder.Entity<User>().HasData(demoUsers);
+         }
 
-        public DbSet<Account> Accounts { get; set; }
-        public DbSet<Log> Logs { get; set; }
-        public DbSet<Security> Securities { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
         public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Security> Securities { get; set; }
+        public DbSet<Log> Logs { get; set; }
+
     }
 }
--- Lumina-Backend/Models/User.cs
+++ Lumina-Backend/Models/User.cs
@@ -4,13 +4,18 @@ public class User : BaseEntity
 {
     public string UserName { get; set; }
     public string Password { get; set; } //Hashed MD5
-    public string SessionToken { get; set; }
+    public string? SessionToken { get; set; }
     public string Email { get; set; }
-    public string FullName { get; set; }
-    public DateTime DateOfBirth { get; set; }
-    public string Address { get; set; }
-    public string ProfileImage { get; set; }
-    public string DNI { get; set; }
+    public string? FullName { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+    public string? Address { get; set; }
+    public string? ProfileImage { get; set; }
+    public string? DNI { get; set; }
+
+    public User()
+    {
+        DateOfBirth = DateTime.SpecifyKind(DateTime.UtcNow, DateTimeKind.Utc);
+    }
 
     public ICollection<Account> Accounts { get; set; }
     public ICollection<Security> Securities { get; set; }
