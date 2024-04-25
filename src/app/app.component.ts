b1ced9969a3995b1ea5060fa267dd861ea4df5ea
Karla Chavez <karla.tkm.96@gmail.com>
2024-04-25 20:29:23 -0300

    agregando landing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './Componentes/header/header.component';
 import { FooterComponent } from './Componentes/footer/footer.component';
-import { PerfilComponent } from './Componentes/home/perfil/perfil.component';
+import { LandingComponent } from './Componentes/landing/landing.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,FooterComponent,PerfilComponent],
+  imports: [RouterOutlet,HeaderComponent,FooterComponent,LandingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
